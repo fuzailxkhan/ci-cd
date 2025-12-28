519805ecff6f8f26c1a0d2485bc3414cb4bbcfea
Fuzail Khan <fuzailxkhan@gmail.com>
2025-12-28 16:37:24 +0500

    feat : pm2 configuration file

---
+++ ecosystem.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  apps: [
+    {
+      name: "ci-cd-app",
+      script: "dist/main.js",
+      instances: 1,
+      exec_mode: "fork",
+      autorestart: true,
+      watch: false,
+      max_memory_restart: "400M",
+
+      env: {
+        NODE_ENV: "development",
+        PORT: 3000
+      },
+
+      env_staging: {
+        NODE_ENV: "staging",
+        PORT: 4000
+      },
+
+      env_production: {
+        NODE_ENV: "production",
+        PORT: 3000
+      }
+    }
+  ]
+};
